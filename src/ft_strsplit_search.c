cf01c1758818ace1d67e6b3c06d93e33044730fa
adpusel <adrien.pusel@gmail.com>
2019-07-15 17:19:13 +0200

    add split_search

---
+++ src/ft_strsplit_search.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adpusel <adpusel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/19 10:48:07 by adpusel           #+#    #+#             */
+/*   Updated: 2017/11/16 12:45:50 by adpusel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strsplit_search(char **split, int(*f)(char *, void *), void *param)
+{
+	if (!split || !*split)
+		return (NULL);
+	while (*split)
+	{
+		if (f(*split, param))
+		    return (*split);
+		split++;
+	}
+	return (NULL);
+}
+
