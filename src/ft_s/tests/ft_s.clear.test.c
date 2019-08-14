913c0d1fbf548510b3f40a32d934f543fd88e339
adpusel <adrien.pusel@gmail.com>
2019-08-15 01:00:51 +0200

    genial le block clear est tester et marche super !

--- src/ft_s/tests/ft_s.clear.test.c
+++ src/ft_s/tests/ft_s.clear.test.c
@@ -10,9 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <test.h>
+#include <libft.test.h>
+#include <ft_s.h>
+#include <ft_mem.h>
 
 void test_ft_$clear()
 {
+	/*
+	* test error handling
+	* */
+	{
+		int ret;
+		g_test = 1;
+		lib_clear_testbuff();
 
+		ret = ft_s$clear(NULL);
+		if (
+			!ret
+			|| lib_cmp_testbuff("ft_s$clear error: s ptr (null)\n")
+			)
+			log_test(0)
+
+		g_test = 0;
+	}
+
+	/*
+	* test good input
+	* */
+	{
+		t_s *s = ft_s$init(0);
+		char *test = ft_memalloc(1000);
+		ft_s$add(&s, "1234567890");
+
+		ft_s$clear(s);
+		if (
+			s->i
+			|| ft_memcmp(test, s->data, s->capacity)
+			)
+		    log_test(1)
+	}
 }
