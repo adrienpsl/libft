92dd9bf7589370b4345e628ad24a16ce66829d0d
adpusel <adpusel@student.42.fr>
2019-09-26 09:11:21 +0200

    norme header ft_array done

--- src/ft_array/includes/ft_array.h
+++ src/ft_array/includes/ft_array.h
@@ -11,100 +11,53 @@
 /* ************************************************************************** */
 
 #ifndef FT_ARRAY_H
-#define FT_ARRAY_H
+# define FT_ARRAY_H
 
-/*
-**	External api
-*/
-#include "unistd.h"
+# include "unistd.h"
+# include "ft_array.structure.h"
 
-/*
-**	Inter Api
-*/
-
-/*
-**	Structure
-*/
-typedef struct s_array
-{
-	int length;
-	int i;
-	int capacity;
-	size_t element_size;
-	char *buffer;
-	char *data;
-} t_array;
-
-/*
-**	Prototypes ******************************************************
-*/
-
-/*
-**	the array size is 2x nb_elements, and will expand like vector.
-*/
-t_array *ftarray__init(int nb_elements, size_t element_size);
-t_array *ftarray__init_data(void *start, int nb_element, size_t element_size);
-t_array *ftarray__slice(t_array *array, int from, int at);
-t_array *ftarray__slice_and_remove(t_array *array, int from, int at);
-t_array *
-ftarray__extract_by_func(t_array *array, int(*f)(void *, void *), void *param);
-t_array *ftarray__copy(t_array *src);
-t_array *ftarray__copy_func(t_array *src, int(*f)(void *, void *, void *),
-	void *param);
-void ftarray__free(t_array **p_array);
-
-void
-ftarray__free_func(t_array **p_array, int (*f)(void *, void *), void *param);
-// utils
-int ftarray__free_func_str(void *element, void *p);
-
-int
-ftarray__remove_func(t_array *array, int index, void (*f)(void *, void *),
-	void *param);
-
-void *ftarray__next(t_array *array);
-void *ftarray__next_loop(t_array *array);
-
-void *ftarray__prev(t_array *array);
-void *ftarray__prev_loop(t_array *array);
-
-void *ftarray__at(t_array *array, int index);
-
-int ftarray__push(t_array *array, void *element);
-int ftarray__insert(t_array *array, void *element, int index);
-int ftarray__remove(t_array *array, int index);
-void ftarray__remove_from(t_array *array, int from, int at);
-
-void *ftarray__func(t_array *array, int(*func)(void *, void *), void *param);
-int ftarray__func_print_str(void *p1, void *param);
-int ftarray__func_print_int(void *p1, void *param);
-
-int ftarray__find(t_array *array, int(*func)(void *, void *), void *param);
-
-/*
-**	--- sort function ----------------------------------------------------------
-*/
-int ftarray__sort_bubble(
-	t_array *array, int(*cmp_f)(void *, void *, void *), void *param
-						);
-
-int ftarray__sort_cmp_int(void *p1, void *p2, void *param);
-
-/*
-**	--- cmp function -----------------------------------------------------------
-*/
-int ftarray__cmp(t_array *array_1, t_array *array_2, int (*f)(void *, void *));
-int ftarray__cmp_int(void *p1, void *p2);
-int ftarray__cmp_str(void *p1, void *p2);
-
-int ftarray__double_size(t_array *array);
-
-/*
-**	utils
-*/
-void ftarray__clear(t_array *array);
-void ftarray__set_start(t_array *array);
-int ftarray__remain(t_array *array);
-void *ftarray__current(t_array *array);
+t_array		*ftarray__init(int nb_elements, size_t element_size);
+t_array		*ftarray__init_data(
+	void *start, int nb_element, size_t element_size);
+t_array		*ftarray__slice(t_array *array, int from, int at);
+t_array		*ftarray__slice_and_remove(t_array *array, int from, int at);
+t_array		*ftarray__extract_by_func(
+	t_array *array, int(*f)(void *, void *), void *param);
+t_array		*ftarray__copy(t_array *src);
+t_array		*ftarray__copy_func(
+	t_array *src, int(*f)(void *, void *, void *), void *param);
+void		ftarray__free(t_array **p_array);
+void		ftarray__free_func(
+	t_array **p_array, int (*f)(void *, void *), void *param);
+int			ftarray__free_func_str(void *element, void *p);
+int			ftarray__remove_func(
+	t_array *array, int index, void (*f)(void *, void *), void *param);
+void		*ftarray__next(t_array *array);
+void		*ftarray__next_loop(t_array *array);
+void		*ftarray__prev(t_array *array);
+void		*ftarray__prev_loop(t_array *array);
+void		*ftarray__at(t_array *array, int index);
+int			ftarray__push(t_array *array, void *element);
+int			ftarray__insert(t_array *array, void *element, int index);
+int			ftarray__remove(t_array *array, int index);
+void		ftarray__remove_from(t_array *array, int from, int at);
+void		*ftarray__func(
+	t_array *array, int(*func)(void *, void *), void *param);
+int			ftarray__func_print_str(void *p1, void *param);
+int			ftarray__func_print_int(void *p1, void *param);
+int			ftarray__find(
+	t_array *array, int(*func)(void *, void *), void *param);
+int			ftarray__sort_bubble(
+	t_array *array, int(*cmp_f)(void *, void *, void *), void *param);
+int			ftarray__sort_cmp_int(void *p1, void *p2, void *param);
+int			ftarray__cmp(
+	t_array *array_1, t_array *array_2, int (*f)(void *, void *));
+int			ftarray__cmp_int(void *p1, void *p2);
+int			ftarray__cmp_str(void *p1, void *p2);
+int			ftarray__double_size(t_array *array);
+void		ftarray__clear(t_array *array);
+void		ftarray__set_start(t_array *array);
+int			ftarray__remain(t_array *array);
+void		*ftarray__current(t_array *array);
 
 #endif
--- src/ft_array/includes/ft_array.structure.h
+++ src/ft_array/includes/ft_array.structure.h
@@ -10,45 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_array.h"
-#include <errno.h>
-#include <ft_log.h>
-#include <ft_printf.h>
+#ifndef FT_ARRAY_STRUCTURE_H
+# define FT_ARRAY_STRUCTURE_H
 
-static int			check(t_array *array, int (*f)(void *, void *))
+typedef struct	s_array
 {
-	if (NULL == array)
-	{
-		return (ftlog__message(F, L,
-				"ftarray__func error: array ptr (null)",
-				EINVAL));
-	}
-	if (NULL == f)
-	{
-		return (ftlog__message(F, L,
-				"ftarray__func error: func ptr (null)",
-				EINVAL));
-	}
-	return (0);
-}
+	int			length;
+	int			i;
+	int			capacity;
+	size_t		element_size;
+	char		*buffer;
+	char		*data;
+}				t_array;
 
-void				*ftarray__func(
-	t_array *array,
-	int (*func)(void *, void *),
-	void *param)
-{
-	if (check(array, func))
-		return (NULL);
-	array->i = 0;
-	while (array->i < array->length)
-	{
-		if (func(ftarray__at(array, array->i), param))
-			return (ftarray__at(array, array->i));
-		array->i++;
-	}
-	if (func == ftarray__func_print_int || func == ftarray__func_print_str)
-	{
-		ft_printf("\n");
-	}
-	return (NULL);
-}
+#endif
