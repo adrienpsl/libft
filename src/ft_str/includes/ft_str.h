0c546de96e542a44a77b79a47c8aadf76a21c542
adpusel <adpusel@student.42.fr>
2019-10-07 17:44:11 +0200

    putchar need to return an int

--- src/ft_str/includes/ft_str.h
+++ src/ft_str/includes/ft_str.h
@@ -48,7 +48,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isascii(int c);
 int		ft_isspace(int c);
-void	ft_putchar(char c);
+int		ft_putchar(char c);
 
 /*
 **	Testing function
--- src/ft_str/src/ft_putchar.c
+++ src/ft_str/src/ft_putchar.c
@@ -12,7 +12,8 @@
 
 #include "libft.h"
 
-void	ft_putchar(char c)
+int		ft_putchar(char c)
 {
 	write(1, &c, 1);
+	return (0);
 }
