262ef1d9c135c5e57671e84322186f28d29b66c7
adpusel <adrien.pusel@gmail.com>
2020-04-04 14:15:54 +0200

    add ft_strcmp.c

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -10,30 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
-# include <unistd.h>
-# include "ft_list.struct.h"
-# include "stdlib.h"
-# include <stdbool.h>
+int ft_strcmp(char *s1, char *s2)
+{
+	size_t i;
 
-ssize_t ft_strchr_int(char *str, char c);
-int ft_isdigit(int c);
-int ft_atoi(const char *str);
-int ft_strlen(const char *str);
-int ft_strcmp(char *s1, char *s2);
-
-void *ft_memcpy(void *dest, const void *src, size_t n);
-void ft_bzero(void *s, size_t n);
-
-t_list *ft_lstnew(const void *content, size_t content_size);
-void ft_lstdel(t_list *p_list, void (*del)(void *, size_t));
-void ft_lstaddend(t_list *lst, t_list *new);
-void ft_lstadd(t_list **lst, t_list *new);
-void	ft_lst_reverse(t_list **lst);
-t_list *ft_listfunc(t_list *list, bool (*f_test)(t_list *, void*), void *ptr);
-
-void print_section_list(t_list *list);
-#endif
+	i = 0;
+	while (s1[i] == s2[i]
+		   && s1[i] && s2[i])
+		i++;
+	return ((unsigned  char)s1[i] - (unsigned char)s2[i]);
+}
