d0cc01b50fd034deeac2c9c41ecf44b30b3c7394
adpusel <adrien.pusel@gmail.com>
2020-04-20 16:05:05 +0200

    add ft_strcmp

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -20,5 +20,20 @@ int ft_strcmp(char *s1, char *s2)
 	while (s1[i] == s2[i]
 		   && s1[i] && s2[i])
 		i++;
-	return ((unsigned  char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int ft_strncmp(char *s1, char *s2, int length)
+{
+	size_t i;
+
+	i = 0;
+	while (s1[i] == s2[i]
+		   && s1[i] && s2[i]
+		   && length)
+	{
+		length -= 1;
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
